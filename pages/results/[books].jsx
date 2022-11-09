719126c77cce78a78251dac956c0b524bc613b9f
sam <7932916+samnj@users.noreply.github.com>
2022-11-08 21:09:30 -0300

    edited loadingNextPageIndicator

--- pages/results/[books].jsx
+++ pages/results/[books].jsx
@@ -50,11 +50,13 @@ const Books = () => {
 			<>
 				<BookList data={data} />
 				<div
-					id='loadingNextPage'
-					className='text-center h-4 mb-4 lg:text-2xl lg:h-10 text-lg animate-pulse text-neutral-800 font-semibold'
+					id='loadingNextPageIndicator'
+					className={`text-center h-4 mb-4 lg:text-2xl lg:h-10 text-lg animate-pulse text-neutral-800 font-semibold ${
+						isFetchingNextPage ? 'visible' : 'invisible'
+					}`}
 					ref={ref}
 				>
-					{isFetchingNextPage ? 'Loading...' : ''}
+					Loading...
 				</div>
 			</>
 		)
